2a26c7ed4cce749a0d8af90b8fd6c02015879d7b
Jonah <jonah.ahvonen@gmail.com>
2022-12-04 18:49:14 +0200

    Solve 1

--- 1/index.js
+++ 1/index.js
@@ -11,16 +11,24 @@ const solve = (initialInput) => {
         }
 
         if(idx === initialInput.length - 1) {
-            elfCalories.push(calory);
+            elfCalories.push(parseInt(calory, 10));
             return;
         }
 
-        elfCalories.push(calory);
+        elfCalories.push(parseInt(calory, 10));
     })
 
     caloriesByElf.push(elfCalories);
 
-    console.log(caloriesByElf);
+    caloriesByElf = caloriesByElf.map(c => c.reduce((a, b) => a + b));
+
+    const m1 = Math.max(...caloriesByElf);
+    caloriesByElf.splice(caloriesByElf.findIndex((v) => v === m1), 1);
+    const m2 = Math.max(...caloriesByElf);
+    caloriesByElf.splice(caloriesByElf.findIndex((v) => v === m2), 1);
+    const m3 = Math.max(...caloriesByElf);
+
+    console.log(m1, m2, m3, m1 + m2 + m3)
 }
 
 export default solve;
